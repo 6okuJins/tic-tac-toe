b6948df60016490c117afcc2aeed3aecff558b19
6okuJins <d.ma@mail.utoronto.ca>
2022-05-11 18:05:13 -0400

    Alternate player turns and game over at 9 rounds

--- script.js
+++ script.js
@@ -19,19 +19,43 @@ const gameBoard = (() => {
 
 const displayController = (() => {
     const buttons = document.querySelectorAll('.game-board>button');
-    
+    const Player1 = Player('X');
+    const Player2 = Player('O');
+
+    let round = 0;
+    let stagedPlayer;
+    let gameOver = false;
+
     buttons.forEach((button) => button.addEventListener('click', () => {
+        if (gameOver) {
+            return
+        }
+        stagedPlayer = checkTurn();
         updateArray(button);
         updateButton(button);
+        round++;
+        gameOver = checkGameOver();
     }));
     function updateArray (button) {
-        gameBoard.set(button.dataset.row, button.dataset.column, "x")
+        gameBoard.set(button.dataset.row, button.dataset.column, stagedPlayer.getMarker());
     }
     function updateButton (button) {
         const text = gameBoard.get()[button.dataset.row][button.dataset.column];
         if (text) {
             button.innerHTML = text;
         }
+    }
+    function checkTurn () {
+        return (round % 2 ? Player2 : Player1)
+    }
+    function checkGameOver () {
+        if (round == 9){
+            return true
+        }
+        return false
+    }
+    function getRound () {
+        return round
     }
     function reset () {
         gameBoard.reset();
@@ -40,7 +64,7 @@ const displayController = (() => {
         });
     }
 
-    return {reset}
+    return {reset, checkGameOver, getRound}
 })();
 
 function Player (marker) {
