43e2012c20ef2d2951c855e123d3f4cd7951b456
6okuJins <d.ma@mail.utoronto.ca>
2022-05-11 21:31:25 -0400

    Add logic to check for complete diagonals and rows

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ const displayController = (() => {
         updateButton(button);
         round++;
         gameOver = checkGameOver();
+        if (gameOver) {
+            console.log(stagedPlayer.getMarker() + " wins!");
+        }
     }));
     function updateArray (button) {
         gameBoard.set(button.dataset.row, button.dataset.column, stagedPlayer.getMarker());
@@ -52,12 +55,49 @@ const displayController = (() => {
         if (round == 9){
             return true
         }
-        return false
+
+        const matrix = gameBoard.get();
+        const transpose =
+        // create a mapping to an array the size of a row from the original matrix. Each element in the array will be a new row.
+        matrix[0].map((_, i) =>
+        // map the ith element of each row from the original matrix to the ith row of transpose
+        matrix.map(row => row[i]));
+        return (checkCompleteRow(matrix) || checkCompleteRow(transpose) || checkCompleteDiagonal(matrix))
     }
     function getRound () {
         return round
     }
+    function checkCompleteRow (matrix) {
+        let result = false;
+        matrix.every(row => {
+            const set = new Set(row);
+            // when converted to a set, a complete row will only contain 1 element
+            if (!set.has(undefined) && set.size == 1) {
+                result = true;
+                return false
+            }
+            return true
+        })
+        return result
+    }
+    function checkCompleteDiagonal (matrix) {
+        console.log('checking diagonal')
+        let result = false;
+        if (matrix[1][1]){
+            console.log('checking diagonal 1')
+            if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) {
+                console.log('checking diagonal 2')
+                result = true;
+            }
+            else if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) {
+                console.log('checking diagonal 3')
+                result = true;
+            }
+        }
+        return result
+    }
     function reset () {
+        round = 0;
         gameBoard.reset();
         buttons.forEach((button) => {
             button.innerHTML = '';
